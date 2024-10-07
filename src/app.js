6a5ebb9c008b9f5784cb74f1a6a9e98c2c665bc4
ptrixp <palpat171@gmail.com>
2024-10-07 00:50:58 -0500

    Pre-Entrega
    Pre-Entrega de productos y carritps de compras para un e-commerce

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"; 
+import productRouter from "./routes/products.router.js";
+import cartRouter from "./routes/carts.router.js";
+//conexion de puerto
+const app = express(); 
+const PUERTO = 8080;
+
+//Middleware: 
+app.use(express.json()); 
+//Le decimos al servidor que vamos a trabajar con JSON. 
+
+//Rutas
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
+
+
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el http://localhost:${PUERTO}`); 
+})
